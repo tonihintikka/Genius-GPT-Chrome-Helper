6f5c21b84fc7caee0e3d0d13ccd29d3531fa824c
Toni Hintikka <toni.hintikka@gmail.com>
2023-04-26 08:35:43 +0300

    update the google manifest version

--- content.js
+++ content.js
@@ -14,6 +14,45 @@ function handleResponse(response) {
     }
   });
 }
+function createDialog(suggestion, textField) {
+    const dialog = document.createElement("div");
+    dialog.id = "chatgpt-dialog";
+    dialog.style.position = "fixed";
+    dialog.style.top = "50%";
+    dialog.style.left = "50%";
+    dialog.style.transform = "translate(-50%, -50%)";
+    dialog.style.backgroundColor = "white";
+    dialog.style.border = "1px solid black";
+    dialog.style.borderRadius = "5px";
+    dialog.style.padding = "20px";
+    dialog.style.zIndex = "10000";
+    dialog.style.width = "400px";
+    dialog.style.boxSizing = "border-box";
+  
+    const suggestionText = document.createElement("p");
+    suggestionText.innerText = suggestion;
+    suggestionText.style.marginBottom = "10px";
+    dialog.appendChild(suggestionText);
+  
+    const pasteButton = document.createElement("button");
+    pasteButton.innerText = "Paste";
+    pasteButton.addEventListener("click", () => {
+      insertSuggestion(suggestion, textField);
+      dialog.remove();
+    });
+    dialog.appendChild(pasteButton);
+  
+    const closeButton = document.createElement("button");
+    closeButton.innerText = "Close";
+    closeButton.style.marginLeft = "10px";
+    closeButton.addEventListener("click", () => {
+      dialog.remove();
+    });
+    dialog.appendChild(closeButton);
+  
+    return dialog;
+  }
+  
 
 function fetchSuggestions(prompt) {
     if (!apiKey) {
@@ -46,17 +85,26 @@ function insertSuggestion(suggestion, textField) {
 }
 
 document.addEventListener("input", async (event) => {
-  const textField = event.target;
-  if (textField.tagName.toLowerCase() === "textarea" || textField.type === "text") {
-    const prompt = textField.value + " /ai";
-    try {
-      const response = await fetchSuggestions(prompt);
-      const suggestion = response.choices[0].text.trim();
-      if (suggestion.length > 0) {
-        insertSuggestion(suggestion, textField);
+    const textField = event.target;
+    if (isTextField(textField)) {
+      const currentValue = getText(textField);
+      const hasAiTrigger = currentValue.match(/^\/ai\s.*\s\/$/);
+  
+      if (hasAiTrigger) {
+        const prompt = currentValue.match(/^\/ai\s(.*\S)\s\/$/)[1]; // Extract prompt text
+        try {
+          const response = await fetchSuggestions(prompt);
+          const suggestion = response.choices[0].text.trim();
+          if (suggestion.length > 0) {
+            const newText = currentValue.replace(/^\/ai\s.*\s\/$/, suggestion);
+            setText(textField, newText);
+            setCursorPosition(textField, newText.length);
+          }
+        } catch (error) {
+          console.error("Error fetching suggestions:", error);
+        }
       }
-    } catch (error) {
-      console.error("Error fetching suggestions:", error);
     }
-  }
-});
+  });
+  
+  
